cce97e8ecd3b727098485dc3d28bfb6321b448ac
Sekiuu <sega.pamma@gmail.com>
2026-03-10 20:31:53 +0700

    hot fix : doesn't import tester in zhuzheeGame

--- OOPgame/src/Core/ZhuzheeGame.java
+++ OOPgame/src/Core/ZhuzheeGame.java
@@ -1,7 +1,7 @@
 package Core;
 
 import Core.GameScreens.MainMenu;
-import Dummy.Map.Map;
+import Dummy.Tester;
 import ZhuzheeEngine.Application;
 import ZhuzheeEngine.ApplicationAdapter;
 import ZhuzheeEngine.Scene.Scene2D;
