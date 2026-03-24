1a184ba7b020a1caac749c7d276c6236df425589
Vinniel <155176879+Vinniel5403@users.noreply.github.com>
2026-03-25 02:14:49 +0700

    remove dragon img

--- OOPgame/src/Core/ZhuzheeGame.java
+++ OOPgame/src/Core/ZhuzheeGame.java
@@ -43,7 +43,7 @@ public class ZhuzheeGame implements ApplicationAdapter {
         OPTION_MENU = new OptionMenu();
         //set current screen
 //        Screen.ChangeScreen(MAIN_MENU);
-        Screen.ChangeScreen(MAIN_MENU);
+        Screen.ChangeScreen(MAIN_SCENE);
 
         //test
 //        Tester.MainMenu(screenManager);
