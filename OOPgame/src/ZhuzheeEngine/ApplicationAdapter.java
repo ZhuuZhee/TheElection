0344f0b8333c838841ad53fc787d4b87b9f63602
GuoyJeng <guojing2549@gmail.com>
2026-03-28 22:51:27 +0700

    delete method that didn't use

--- OOPgame/src/ZhuzheeEngine/ApplicationAdapter.java
+++ OOPgame/src/ZhuzheeEngine/ApplicationAdapter.java
@@ -2,7 +2,7 @@ package ZhuzheeEngine;
 
 public interface ApplicationAdapter {
     public void create();
-    public void resize(int width, int height);
+//    public void resize(int width, int height);
     public void render();
     public void dispose();
 }
